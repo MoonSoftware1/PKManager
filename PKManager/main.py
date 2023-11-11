d7bd19b3dfdb0366094f649232993c8017633f53
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-11-11 20:46:50 +0100

    Fix bug

--- PKManager/main.py
+++ PKManager/main.py
@@ -1,8 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from application.templates.home import HOMETemplate
+from application.packages.log import logs
 
 if __name__ == '__main__':
+    logs("Launch the application.")
     application = QApplication(sys.argv)
     root = HOMETemplate()
     root.show()
